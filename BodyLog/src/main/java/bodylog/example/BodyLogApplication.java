05d6657dedd273184ee0c235ad54b8d03e9c77e0
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-05-17 23:55:41 +0900

    [All] add : 스프링 부트 프로젝트 생성

---
+++ BodyLog/src/main/java/bodylog/example/BodyLogApplication.java
@@ -0,0 +1,13 @@
+package bodylog.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyLogApplication.class, args);
+	}
+
+}
---
+++ BodyLog/src/test/java/bodylog/example/BodyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package bodylog.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
