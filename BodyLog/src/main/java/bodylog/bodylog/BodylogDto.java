c84dbb1caf9cb23fad819af9ab57fbbc2439f482
chacha <garam_angel@naver.com>
2021-05-18 23:50:59 +0900

    [BodylogDto] add: 바디로그dto 생성 (수정 가능성 있음)

--- BodyLog/src/main/java/bodylog/bodylog/BodylogDto.java
+++ BodyLog/src/main/java/bodylog/bodylog/BodylogDto.java
@@ -1,5 +1,58 @@
 package bodylog.bodylog;
 
+import java.util.Date;
+
 public class BodylogDto {
+	private int member_num; 
+	private String cal_content; 
+	private String cal_img; 
+	private Date cal_startday;
+	private Date cal_endday;
+	
+	public BodylogDto() {
+	}
+	
+	public BodylogDto(int member_num, String cal_content, String cal_img, Date cal_startday, Date cal_endday) {
+		this.member_num = member_num;
+		this.cal_content = cal_content;
+		this.cal_img = cal_img;
+		this.cal_startday = cal_startday;
+		this.cal_endday = cal_endday;
+	}
+	
+	public int getMember_num() {
+		return member_num;
+	}
+	
+	public void setMember_num(int member_num) {
+		this.member_num = member_num;
+	}
+	public String getCal_content() {
+		return cal_content;
+	}
+	public void setCal_content(String cal_content) {
+		this.cal_content = cal_content;
+	}
+	public String getCal_img() {
+		return cal_img;
+	}
+	public void setCal_img(String cal_img) {
+		this.cal_img = cal_img;
+	}
+	public Date getCal_startday() {
+		return cal_startday;
+	}
+	public void setCal_startday(Date cal_startday) {
+		this.cal_startday = cal_startday;
+	}
+	public Date getCal_endday() {
+		return cal_endday;
+	}
+	public void setCal_endday(Date cal_endday) {
+		this.cal_endday = cal_endday;
+	} 
+	
+	
+	
 
 }
