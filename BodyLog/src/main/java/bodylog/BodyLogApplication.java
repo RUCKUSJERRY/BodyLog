3092ffcd3934ce60a80fda1fd175314fa5042b2b
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-05-20 00:33:22 +0900

    [common] mod : 타임리프 의존성 추가 및 프로퍼티스 수정

--- BodyLog/src/main/java/bodylog/BodyLogApplication.java
+++ BodyLog/src/main/java/bodylog/BodyLogApplication.java
@@ -1,4 +1,4 @@
-package bodylog.example;
+package bodylog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BodyLog/src/test/java/bodylog/BodyLogApplicationTests.java
+++ BodyLog/src/test/java/bodylog/BodyLogApplicationTests.java
@@ -1,4 +1,4 @@
-package bodylog.example;
+package bodylog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
