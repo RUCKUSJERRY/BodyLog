3141f474d58296398fa7b06be45b276a560ab3fa
chacha <garam_angel@naver.com>
2021-05-24 21:29:42 +0900

    [기타]etc : merge  를 위한 커밋입니다

--- BodyLog/src/main/java/bodylog/bodylog/BodylogMapper.java
+++ BodyLog/src/main/java/bodylog/bodylog/BodylogMapper.java
@@ -12,18 +12,18 @@ import org.apache.ibatis.annotations.Update;
 public interface BodylogMapper {
 	
 	@Insert("INSERT INTO BODYLOG VALUES(LOG_SEQ.NEXTVAL,1000,#{log_content},#{log_img},#{log_startday},#{log_endday})")
-	public int insert(BodylogDto dto);
+	int insert(BodylogDto dto);
 	
 	@Select("SELECT LOG_SEQ, LOG_CONTENT, LOG_IMG, LOG_STARTDAY, LOG_ENDDAY FROM BODYLOG WHERE MEMBER_NUM=1000")
-	public List<BodylogDto> selectList();
+	List<BodylogDto> selectList();
 	
 	@Select("SELECT LOG_SEQ,LOG_CONTENT,LOG_IMG,LOG_STARTDAY,LOG_ENDDAY FROM BODYLOG WHERE LOG_SEQ=#{log_seq}")
-	public BodylogDto selectOne(int log_seq);
+	BodylogDto selectOne(int log_seq);
 	
-	@Update("UPDATE BODYLOG SET LOG_CONTENT=#{log_content},LOG_IMG=#{log_img},LOG_STARTDAY=#{log_startday},LOG_ENDDAY=#{log_endday} WHERE LOG_SEQ=#{log_seq}")
-	public int update(BodylogDto dto);
+	@Update("UPDATE BODYLOG SET LOG_CONTENT= #{log_content},LOG_IMG = #{log_img},LOG_STARTDAY = #{log_startday},LOG_ENDDAY = #{log_endday} WHERE LOG_SEQ = #{log_seq}")
+	int update(BodylogDto dto);
 	
 	@Delete("DELETE FROM BODYLOG WHERE LOG_SEQ=#{log_seq}")
-	public int delete(int log_seq);
+	int delete(int log_seq);
 
 }
